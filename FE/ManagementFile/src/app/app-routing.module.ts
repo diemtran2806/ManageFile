accf8084f79cc8ebf7e77afb6d70ab2026a4f4ef
vanthien64 <vanthiendiendien@gmail.com>
2023-10-04 14:55:05 +0700

    ft:Design UI for view function

--- FE/ManagementFile/src/app/app-routing.module.ts
+++ FE/ManagementFile/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { View } from './View/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'view', component:View}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FE/ManagementFile/src/app/app.module.ts
+++ FE/ManagementFile/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { View } from './View/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    View
   ],
   imports: [
     BrowserModule,
