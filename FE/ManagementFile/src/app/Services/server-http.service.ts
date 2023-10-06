f2722e165e6d83af0dfffa94c10b0f054c0776b8
diemtran2806 <ttdiem2002@gmail.com>
2023-10-06 09:29:44 +0700

    Integrate upload file

--- FE/ManagementFile/src/app/Services/server-http.service.ts
+++ FE/ManagementFile/src/app/Services/server-http.service.ts
@@ -11,7 +11,7 @@ import {
 })
 export class ServerHttpService {
   
-  private REST_API_SERVER = 'https://localhost:7096/api';
+  private REST_API_SERVER = 'https://localhost:5050/api';
   getAllFile() {
     return axios.get(`${this.REST_API_SERVER}/file`);
   }
@@ -44,20 +44,4 @@ export class ServerHttpService {
       },
     })
   }
-
-  // private handleError(error: HttpErrorResponse) {
-  //   if (error.error instanceof ErrorEvent) {
-  //     // A client-side or network error occurred. Handle it accordingly.
-  //     console.error('An error occurred:', error.error.message);
-  //   } else {
-  //     // The backend returned an unsuccessful response code.
-  //     // The response body may contain clues as to what went wrong,
-  //     console.error(
-  //       `Backend returned code ${error.status}, ` + `body was: ${error.error}`
-  //     );
-  //   }
-  //   // return an observable with a user-facing error message
-  //   return throwError('Something bad happened; please try again later.');
-  // }
-
 }
